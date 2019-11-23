8d13ffc273c3adf12ecfa3a0ceb9ad5e916eab8d
Aman Bhardwaj <amanbhardwaj.04091997@gmail.com>
2019-11-23 12:44:23 +0530

    First Linked List Creation

---
+++ The Creation of Linked List/The Creation of Linked List/main.c
@@ -0,0 +1,55 @@
+//
+//  main.c
+//  The Creation of Linked List
+//
+//  Created by Aman Bhardwaj on 11/23/19.
+//  Copyright © 2019 Projects. All rights reserved.
+//
+
+#include<stdlib.h>
+#include<stdio.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+} *first;
+
+void Create(int A[], int n)
+{
+    int i;
+    struct Node *t;
+    struct Node *last;
+    
+    first = (struct Node *)malloc(sizeof(struct Node));
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+    for(i=1;i<n;i++)
+    {
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+void Display(struct Node *p)
+{
+    while(p)
+    {
+        printf(" %d ", p->data);
+        p = p->next;
+    }
+}
+
+
+int main()
+{
+    int A[5] = {1,2,3,4,5};
+    Create(A,5);
+    Display(first);
+    return 0;
+    
+}
